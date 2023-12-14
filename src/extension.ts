463f0b5a2c83fa50ea18da143167629f0efdf2e9
Phill Johntony <pdjohntony@gmail.com>
2023-12-13 22:01:50 -0500

    decorator wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,64 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vcrpy-mgr" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vcrpy-mgr.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let command1 = vscode.commands.registerCommand('vcrpy-mgr.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from vcrpy-mgr and Phill!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(command1);
+
+	updateDecorations(vscode.window.activeTextEditor, context);
+
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+		updateDecorations(editor, context);
+	}));
+
+	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
+		updateDecorations(vscode.window.activeTextEditor, context);
+	}));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function updateDecorations(editor?: vscode.TextEditor, context?: vscode.ExtensionContext) {
+	if (!editor || !context) {
+		return;
+	}
+	if (editor.document.languageId !== 'python') {
+		return;
+	}
+	// ? this isn't working
+	// if (!editor.document.fileName.startsWith('test')) {
+	// 	return;
+	// }
+	console.log('Updating vcrpy decorations');
+
+	const text = editor.document.getText();
+	
+	const vcrDecorations: vscode.DecorationOptions[] = [];
+	const regex = /@pytest\.mark\.vcr/g;
+	let match;
+	while (match = regex.exec(text)) {
+		const startPos = editor.document.positionAt(match.index+1);
+		const endPos = editor.document.positionAt(match.index + match[0].length);
+		const decoration = { range: new vscode.Range(startPos, endPos) };
+		vcrDecorations.push(decoration);
+		console.log('Found pytest vcrpy decorator');
+	}
+
+	const vcrDecorationType = vscode.window.createTextEditorDecorationType({
+		gutterIconPath: vscode.Uri.file(context.asAbsolutePath('src/cassette-fill.svg')),
+		gutterIconSize: 'auto',
+		after: {
+			contentText: 'Possible cassette',
+			margin: '0 0 0 2em',
+			color: '#ff8040',
+		},
+	});
+
+	editor.setDecorations(vcrDecorationType, vcrDecorations);
+	console.log('Updated vcrpy decorations');
+}
